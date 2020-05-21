6b7fc2ae5e5e6faca154485c445ff9dc4edbfc35
Nina Tan <ninavtan@gmail.com>
2020-05-20 21:18:51 -0600

    first commit

---
+++ astro-app.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"))
+
+
+app.get("/", function(req, res){
+	res.render("home");
+});
+
+
+app.get("/results", function(req, res){
+	var sign = req.query.sign;
+	var day = "today";
+	
+	var options = {
+	method: 'POST',
+	url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
+	qs: 
+			{
+				sign: sign, 
+				day: day
+			},
+	headers: {
+	'x-rapidapi-host': 'sameer-kumar-aztro-v1.p.rapidapi.com',
+	'x-rapidapi-key': '6ab23d37fcmsh1ea2038c95f42adp1bb70fjsnf6c931dec9f1',
+	'content-type': 'application/x-www-form-urlencoded',
+	useQueryString: true
+	},
+	form: {}
+	};
+	
+	request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	
+		var data = JSON.parse(body);
+		console.log(body);
+		res.render("results", {data: data});
+		
+});
+});
+
+////
+
+// 
+
+// take variables and send to api 
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() { 
+  console.log('Astro App has started.') 
+});
